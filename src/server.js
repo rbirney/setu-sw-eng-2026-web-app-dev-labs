dec8df46191e4bb8137001544fba78c48529ae58
rbirney <rbirney@wit.ie>
2026-09-07 11:16:47 +0100

    Lab 1c - MVC Controllers and Routes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import Hapi from "@hapi/hapi";
 import path from "path";
 
 import { fileURLToPath } from "url";
+import { webRoutes } from "./web-routes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,6 +12,9 @@ async function init() {
     port: 3000,
     host: "localhost",
   });
+  
+  server.route(webRoutes);
+  
   await server.start();
   console.log("Server running on %s", server.info.uri);
 }
