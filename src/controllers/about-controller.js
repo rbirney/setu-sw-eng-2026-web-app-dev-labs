9d5aface3675a26e6e41980f954e9cd7b2a476e1
rbirney <rbirney@wit.ie>
2026-09-07 11:18:07 +0100

    Lab 1d - Exercise One solution

---
+++ src/controllers/about-controller.js
@@ -0,0 +1,7 @@
+export const aboutController = {
+  index: {
+    handler: async function (request, h) {
+      return h.response("About the Playtime App");
+    },
+  },
+};
--- src/web-routes.js
+++ src/web-routes.js
@@ -1,5 +1,7 @@
 import { dashboardController } from "./controllers/dashboard-controller.js";
+import { aboutController } from "./controllers/about-controller.js";
 
 export const webRoutes = [
-    { method: "GET", path: "/", config: dashboardController.index }
+    { method: "GET", path: "/", config: dashboardController.index },
+    { method: "GET", path: "/about", config: aboutController.index }
 ];
