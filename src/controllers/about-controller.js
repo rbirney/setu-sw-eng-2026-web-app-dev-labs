9637ea7dd47fbfb371d5a20e59f5eb87e44c6874
rbirney <rbirney@wit.ie>
2026-09-08 12:17:42 +0100

    Lab 1f - Exercise Two solution

--- src/controllers/about-controller.js
+++ src/controllers/about-controller.js
@@ -1,7 +1,7 @@
 export const aboutController = {
   index: {
     handler: async function (request, h) {
-      return h.response("About the Playtime App");
+      return h.view("about");
     },
   },
 };
